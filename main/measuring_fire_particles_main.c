4ab7ba499e07467cdf3855b5c3678679495616c0
Elin Björck <eb222wb@student.lnu.se>
2023-02-21 18:15:47 +0100

    Strugling

--- main/measuring_fire_particles_main.c
+++ main/measuring_fire_particles_main.c
@@ -11,13 +11,24 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
-#include "driver/gpio.h"
+#include "driver/uart.h"
 
-#include "esp_sds011-1.1.0/src/Sds011.h"
+#define BUF_SIZE (1024)
 
 void app_main()
 {
     printf("Hello world!\n");
+
+    uart_config_t uart_config = {
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    }; 
+    
+    uart_param_config(UART_NUM_0, &uart_config);
+    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -30,22 +41,13 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
     
-    int counter = 0;
-
-    gpio_config_t pin_config;
-    pin_config.intr_type = GPIO_INTR_DISABLE;
-    pin_config.mode = GPIO_MODE_OUTPUT;
-    pin_config.pin_bit_mask = (1ULL<<5);
-    pin_config.pull_down_en = 0;
-    pin_config.pull_up_en = 0;
-    gpio_config(&pin_config);
 
     for (int i = 10; i >= 0; i--) {
-        counter++;
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        gpio_set_level(5, counter%2);
     }
+    uart_write_bytes()
+
     printf("Restarting now.\n");
     fflush(stdout);
     esp_restart();
