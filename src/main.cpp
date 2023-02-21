8fae40538ebdf3a1606fad810f71a2a7c9ab1bf3
Elin Björck <eb222wb@student.lnu.se>
2023-02-21 19:53:05 +0100

    sensor library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#include "SdsDustSensor.h"
 
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
+  while(1) {
+    SdsDustSensor.
+  }
   // put your main code here, to run repeatedly:
 }
