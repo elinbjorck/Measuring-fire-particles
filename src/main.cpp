8ecf7ce13b33a4cd448fd5e8fc94164e98bc56fe
Elin Björck <eb222wb@student.lnu.se>
2023-02-22 09:00:59 +0100

    It's working :'D

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
 #include <Arduino.h>
 #include "SdsDustSensor.h"
 
+SdsDustSensor sds(Serial2);
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  delay(500);
+  
+  Serial.println("Hello from ESP32!");
+
+  sds.begin();
+
+  Serial.println(sds.queryFirmwareVersion().toString()); 
+  sds.setQueryReportingMode();
 }
 
 void loop() {
-  while(1) {
-    SdsDustSensor.
-  }
+  sds.wakeup();
+  sleep(2);
+  PmResult ressult =  sds.queryPm();
+  Serial.println(ressult.pm25);
+  sds.sleep();
+  sleep(2);
   // put your main code here, to run repeatedly:
 }
